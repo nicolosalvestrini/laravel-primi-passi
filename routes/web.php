59b357112ce0ecd2192fc4e4be97b8f3b0b42245
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-06-28 23:53:47 +0200

    refactor: consolidate navigation links definition in routes

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,20 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $navlinks = [
-        ['name' => 'Home', 'url' => '/'],
-        ['name' => 'About', 'url' => '/about'],
-        ['name' => 'Contact', 'url' => '/contact'],
-    ];
+$navlinks = [
+    ['name' => 'Home', 'url' => '/'],
+    ['name' => 'About', 'url' => '/about'],
+    ['name' => 'Contact', 'url' => '/contact'],
+];
 
+Route::get('/', function () use ($navlinks) {
     return view('home', compact('navlinks'));
 });
 
-Route::get('/about', function () {
-    $navlinks = [
-        ['name' => 'Home', 'url' => '/'],
-        ['name' => 'About', 'url' => '/about'],
-        ['name' => 'Contact', 'url' => '/contact'],
-    ];
-
+Route::get('/about', function () use ($navlinks) {
     return view('about', compact('navlinks'));
 });
 
-Route::get('/contact', function () {
-    $navlinks = [
-        ['name' => 'Home', 'url' => '/'],
-        ['name' => 'About', 'url' => '/about'],
-        ['name' => 'Contact', 'url' => '/contact'],
-    ];
-
+Route::get('/contact', function () use ($navlinks) {
     return view('contact', compact('navlinks'));
 });
