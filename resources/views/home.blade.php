bdb6af25451922fb409b56b7073008d6e092a3ee
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-06-28 23:11:57 +0200

    set up for ex

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+</head>
+<body>
+    <header>
+        <h1>Hello World</h1>
+    </header>
+    
+</body>
+</html>
