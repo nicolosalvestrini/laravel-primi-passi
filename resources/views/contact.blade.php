b3848fb6e70659c3b7c5aed7d442858539365fb7
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-06-28 23:50:24 +0200

    fix: update language attribute and titles in about and contact pages

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Contact</title>
 </head>
 <body>
     <header class="bg-dark text-white py-3">
